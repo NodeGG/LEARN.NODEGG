52be61124fe598170d259df03b8b61e65a1e10c5
Warren Gates <warrenckhan@gmail.com>
2019-01-29 22:46:59 -0500

    Patch 0.0.1.5 (#5)
    Removed unused packages and files/code. added ggtyle sheet to page and removed
    fixed styling of guide page to be more user friendly

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="fnt-monospace">
     <div id="nav">
       <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-        <router-link to='/' class="navbar-brand">Node Codes</router-link>
+        <router-link to='/' class="navbar-brand">LEARN.NODEGG</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -20,6 +20,9 @@
             <li class="nav-item">
               <router-link class="nav-link" to="/about">About</router-link>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/NodeGG/nodecodes" target="_blank">Help Us!</a>
+            </li>
           </ul>
         </div>
       </nav>
@@ -27,9 +30,8 @@
     <router-view class="mt-5"/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  height:inherit
-}
+<style>
+  #app{
+    margin-top: 50px;
+  }
 </style>

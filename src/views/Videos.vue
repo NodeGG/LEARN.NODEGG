d2ebf1c0ecba18d510a6e369c90ed8b40319c4c1
Warren Gates <warrenckhan@gmail.com>
2019-01-30 13:09:07 -0500

    Add to Project README Changed Card font to 17 added botttom border to index item and remove <hr/> tag
    added <hr/> tag in between getting started and video

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -3,6 +3,7 @@
   <div class="video-section-one mt-5 mx-2">
     <div class="title-section">
       <h4 class="mt-5 mb-2 pt-3">Getting Started</h4>
+      <hr class="w-75"/>
     </div>
     <div class="video-section py-5 mx-auto px-2">
       <iframe width="100%" height="100%" src="https://www.youtube.com/embed/videoseries?list=PLWRXkzqC3r2-OLixVYMKx64D9KJI9PkBX" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -17,11 +18,10 @@ export default {
 }
 </script>
 <style>
-.video-section {
-  max-width: 800px;
-  min-width: 300px;
-  height: 35vw;
-  min-height: 500px
-}
-
+  .video-section {
+    max-width: 800px;
+    min-width: 300px;
+    height: 35vw;
+    min-height: 500px
+  }
 </style>
